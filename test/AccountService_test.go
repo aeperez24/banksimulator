2fd112db420d2c77d5376ee7799cc699894d3388
andres <aperez@kdu.cl>
2021-10-02 13:20:47 -0300

    adding transfer functionality

--- test/AccountService_test.go
+++ test/AccountService_test.go
@@ -22,11 +22,20 @@ func (a AccountRepositoryMock) ModifyBalanceForAccount(accountNumber string, amo
 func getAccountRepositoryMock() model.AccountRepository {
 	accountsMap := make(map[string]model.Account)
 	accountsMap["1"] = model.Account{AccountNumber: "1", Balance: 100}
-	accountsMap["2"] = model.Account{AccountNumber: "2", Balance: 100}
+	accountsMap["2"] = model.Account{AccountNumber: "2", Balance: 200}
 	findAccountByAccountNumber := func(number string) model.Account {
 		return accountsMap[number]
 	}
-	return AccountRepositoryMock{FindAccountByAccountNumberFn: findAccountByAccountNumber}
+	modifyBalance := func(accountNumber string, amount float32) bool {
+
+		account := accountsMap[accountNumber]
+		account.Balance = account.Balance + amount
+		accountsMap[accountNumber] = account
+
+		return true
+	}
+	return AccountRepositoryMock{FindAccountByAccountNumberFn: findAccountByAccountNumber,
+		ModifyBalanceForAccountFn: modifyBalance}
 }
 
 func TestGetBalance(t *testing.T) {
@@ -36,6 +45,51 @@ func TestGetBalance(t *testing.T) {
 	expected := 100
 	if 100 != result {
 		t.Errorf("expected %v and received %v", expected, result)
+	}
+
+}
+
+func TestMustTransferAmountSuccesfully(t *testing.T) {
+	mock := getAccountRepositoryMock()
+	service := services.NewAccountService("1", mock)
+	result := service.TransferMoneyTo("2", 50)
+	balanceAccount1 := mock.FindAccountByAccountNumber("1").Balance
+	balanceAccount2 := mock.FindAccountByAccountNumber("2").Balance
+	expectedBalanceAccount1 := float32(50)
+	expectedBalanceAccount2 := float32(250)
+	expectedResult := true
+	if expectedBalanceAccount1 != balanceAccount1 {
+		t.Errorf("expected %v and received %v", expectedBalanceAccount1, balanceAccount1)
+	}
+	if expectedBalanceAccount2 != balanceAccount2 {
+		t.Errorf("expected %v and received %v", expectedBalanceAccount2, balanceAccount2)
+	}
+
+	if expectedResult != result {
+		t.Errorf("expected %v and received %v", expectedResult, result)
+	}
+
+}
+
+func TestMustNotTransferAmountWhenIsGreaterThanBalance(t *testing.T) {
+	mock := getAccountRepositoryMock()
+	service := services.NewAccountService("1", mock)
+	result := service.TransferMoneyTo("2", 500)
+	balanceAccount1 := mock.FindAccountByAccountNumber("1").Balance
+	balanceAccount2 := mock.FindAccountByAccountNumber("2").Balance
+	expectedBalanceAccount1 := float32(100)
+	expectedBalanceAccount2 := float32(200)
+	expectedResult := false
+
+	if expectedBalanceAccount1 != balanceAccount1 {
+		t.Errorf("expected %v and received %v", expectedBalanceAccount1, balanceAccount1)
+	}
+	if expectedBalanceAccount2 != balanceAccount2 {
+		t.Errorf("expected %v and received %v", expectedBalanceAccount2, balanceAccount2)
+	}
+
+	if expectedResult != result {
+		t.Errorf("expected %v and received %v", expectedResult, result)
 	}
 
 }
